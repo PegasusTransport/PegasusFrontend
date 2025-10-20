17631a765499a39898ad3c694d527851bc165e64
Tim Suvorov <timsuvorov@icloud.com>
2025-10-20 20:34:58 +0200

    refactor: update registration form fields and labels for clarity

--- pegasus-frontend/src/components/auth/RegisterForm.vue
+++ pegasus-frontend/src/components/auth/RegisterForm.vue
@@ -27,93 +27,165 @@ const register = async () => {
   }
   try {
     const response = await authApi.register(registerForm.value);
-    console.log(response.data);   
-console.log(response.message); 
+    console.log(response.data);
+    console.log(response.message);
     message.value = `Anändare är registrerad. Namn: ${response.data.firstName}`;
   } catch (error: any) {
     message.value =
-     error.response?.data?.message ||
+      error.response?.data?.message ||
       "Det gick inte att registrera en användare";
   }
 };
 </script>
 
 <template>
-  <div class="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+  <div
+    class="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8"
+  >
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      <img class="mx-auto h-10 w-auto" src="/src/assets/img/Pegasus.png" alt="Your Company" />
-      <h2 class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Registrera dig</h2>
+      <img
+        class="mx-auto h-10 w-auto"
+        src="/src/assets/img/Pegasus.png"
+        alt="Your Company"
+      />
+      <h2
+        class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900"
+      >
+        Registrera dig på Pegasus Transport
+      </h2>
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
       <div class="bg-white px-6 py-12 shadow-sm sm:rounded-lg sm:px-12">
-        <form @submit.prevent="register" class="space-y-6" >
+        <form @submit.prevent="register" class="space-y-6">
           <div>
-            <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+            <label for="email" class="block text-sm/6 font-medium text-gray-900"
+              >Användarnamn</label
+            >
             <div class="mt-2">
-              <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              <input
+                v-model="registerForm.userName"
+                type="text"
+                required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
             </div>
           </div>
 
           <div>
-            <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+            <label for="text" class="block text-sm/6 font-medium text-gray-900"
+              >Förnamn</label
+            >
             <div class="mt-2">
-              <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              <input
+                v-model="registerForm.firstName"
+                type="text"
+                autocomplete="name"
+                required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
             </div>
           </div>
 
           <div>
-            <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+            <label for="text" class="block text-sm/6 font-medium text-gray-900"
+              >Efternamn</label
+            >
             <div class="mt-2">
-              <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              <input
+                v-model="registerForm.lastName"
+                type="text"
+                autocomplete="family-name"
+                required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
             </div>
           </div>
 
           <div>
-            <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+            <label for="phone" class="block text-sm/6 font-medium text-gray-900"
+              >Telefonnummer</label
+            >
             <div class="mt-2">
-              <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              <input
+                v-model="registerForm.phoneNumber"
+                type="tel"
+                autocomplete="tel"
+                required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
             </div>
           </div>
 
           <div>
-            <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+            <label for="email" class="block text-sm/6 font-medium text-gray-900"
+              >Email address</label
+            >
             <div class="mt-2">
-              <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-            </div>
-          </div>
-
-          <div class="flex items-center justify-between">
-            <div class="flex gap-3">
-              <div class="flex h-6 shrink-0 items-center">
-                <div class="group grid size-4 grid-cols-1">
-                  <input id="remember-me" name="remember-me" type="checkbox" class="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto" />
-                  <svg class="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25" viewBox="0 0 14 14" fill="none">
-                    <path class="opacity-0 group-has-checked:opacity-100" d="M3 8L6 11L11 3.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path class="opacity-0 group-has-indeterminate:opacity-100" d="M3 7H11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </div>
-              </div>
-              <label for="remember-me" class="block text-sm/6 text-gray-900">Remember me</label>
-            </div>
-
-            <div class="text-sm/6">
-              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+              <input
+                v-model="registerForm.email"
+                type="email"
+                name="email"
+                id="email"
+                autocomplete="email"
+                required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
             </div>
           </div>
 
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Registera</button>
+            <label
+              for="password"
+              class="block text-sm/6 font-medium text-gray-900"
+              >Lösenord</label
+            >
+            <div class="mt-2">
+              <input
+                v-model="registerForm.password"
+                type="password"
+                name="password"
+                id="password"
+                required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              for="password"
+              class="block text-sm/6 font-medium text-gray-900"
+              >Konfirmera Lösenord</label
+            >
+            <div class="mt-2">
+              <input
+                v-model="registerForm.confirmPassword"
+                type="password"
+                name="password"
+                id="confipassword"
+                required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Registera
+            </button>
           </div>
         </form>
-
-        
       </div>
 
       <p class="mt-10 text-center text-sm/6 text-gray-500">
-        Not a member?
-        {{ ' ' }}
-        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+        Har du redan ett konto?
+        {{ " " }}
+        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500"
+          >Logga in</a
+        >
       </p>
     </div>
   </div>
