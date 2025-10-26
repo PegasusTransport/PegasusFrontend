bd7b08357312e5027ab8afd1d205a72a9a962888
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-10-26 17:38:12 +0100

    feat: Button can now be a link or a regular button. Button can now accept custom classes.

--- pegasus-frontend/src/components/reusables/Button.vue
+++ pegasus-frontend/src/components/reusables/Button.vue
@@ -1,8 +1,27 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    link?: boolean;
+    to?: string;
+    buttonClass?: string;
+  }>(),
+  { link: false, buttonClass: "" }
+);
+
+const setButtonClass = computed(
+  () =>
+    props.buttonClass ||
+    "rounded-md cursor-pointer bg-pg-persian px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pg-accent focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors duration-300"
+);
+</script>
+
 <template>
-  <button
-    type="button"
-    class="rounded-md cursor-pointer bg-pg-persian px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pg-accent focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors duration-300"
-  >
+  <button v-if="!link" type="button" :class="setButtonClass">
     <slot></slot>
   </button>
+  <RouterLink v-else :to="to!" :class="setButtonClass">
+    <slot></slot>
+  </RouterLink>
 </template>
