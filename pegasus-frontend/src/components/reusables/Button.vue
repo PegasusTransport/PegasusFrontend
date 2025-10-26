95d90a11b3908261066f31ac89a7518dc32e274e
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-10-26 13:55:39 +0100

    feat: Added color transition to 'Button.vue'

--- pegasus-frontend/src/components/reusables/Button.vue
+++ pegasus-frontend/src/components/reusables/Button.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="button"
-    class="rounded-md cursor-pointer bg-pg-persian px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pg-accent  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    class="rounded-md cursor-pointer bg-pg-persian px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pg-accent focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors duration-300"
   >
     <slot></slot>
   </button>
