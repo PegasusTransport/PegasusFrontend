b5e8290003ff35682157f92c208d79fc6c604c5c
Tim Suvorov <timsuvorov@icloud.com>
2025-11-03 11:26:07 +0100

    57 UI för uppdater användarens profil (#68)
    * fix: added a full height and color on body and html
    
    * fix: adjust spacing in registration form and text input label for improved layout
    
    * feat: update CustomerProfile to include ProfileSettings component
    
    * feat: enhance TaxiPrices component with improved NumberInput labels and validation
    
    * feat: add ProfileSettings component for user profile management
    
    * feat: Add user profile fetching and form population in ProfileSettings component
    
    ---------
    
    Co-authored-by: Tim Suvorov <timiniev@outlook.com>

--- pegasus-frontend/src/endpoints/user.ts
+++ pegasus-frontend/src/endpoints/user.ts
@@ -2,14 +2,21 @@ import api from "@/plugins/axios";
 import type { ApiResponse } from "@/types/api-response-dto";
 import type { UpdateUserRequestDto } from "@/types/update-user-request-dto";
 import type { UpdateUserResponseDto } from "@/types/update-user-response-dto";
+import type { UserResponseDto } from "@/types/user-response-dto";
 
 export const userApi = {
   async updateProfile(
     data: UpdateUserRequestDto
   ): Promise<ApiResponse<UpdateUserResponseDto>> {
-    const response = await api.post<ApiResponse<UpdateUserResponseDto>>(
+    const response = await api.put<ApiResponse<UpdateUserResponseDto>>(
       "/api/User/UpdateUser",
       data
+    );
+    return response.data;
+  },
+  async getUserProfile(): Promise<ApiResponse<UserResponseDto>> {
+    const response = await api.get<ApiResponse<UserResponseDto>>(
+      "/api/User/GetLoggedInUserData"
     );
     return response.data;
   },
