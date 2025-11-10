309833409f2dcb0ab87ee73ad81e2ef16bf52714
Tim Suvorov <timiniev@outlook.com>
2025-11-10 13:26:38 +0100

    feat: implemented new jobs for drivers, theyre able to accept and see the trajectory

--- pegasus-frontend/src/pages/Driver/DriverBookings.vue
+++ pegasus-frontend/src/pages/Driver/DriverBookings.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <h1>Här kan en förare sina tidigare resor, avslutar eller acceptera nya resor om bokning is available då ser alla förare det och sen egen historik</h1>
+  
 </template>
