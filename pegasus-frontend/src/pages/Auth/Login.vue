9bf47f916066edd41340d68d8ef7ae4a124dad1b
Tim Suvorov <timiniev@outlook.com>
2025-11-11 21:58:46 +0100

    fix: correct padding in Login.vue for no scroll on mobile version

--- pegasus-frontend/src/pages/Auth/Login.vue
+++ pegasus-frontend/src/pages/Auth/Login.vue
@@ -3,7 +3,7 @@ import LoginForm from "@/components/auth/login/LoginForm.vue";
 </script>
 
 <template>
-  <div class="bg-pg-primary md:p-0 p-4">
+  <div class="bg-pg-primary md:p-0 px-4">
     <LoginForm />
   </div>
 </template>
